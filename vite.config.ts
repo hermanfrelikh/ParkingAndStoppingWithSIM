e74ff5338ce961fb1a214584c91145ee042abdb7
Герман <german@MacBook-Air-German.local>
2026-02-21 23:18:00 +0300

    навбар сдела с Liquid Glass

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 /// <reference types="vitest/config" />
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-import { fileURLToPath } from "url";
-import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
-import svgr from "vite-plugin-svgr";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import svgr from 'vite-plugin-svgr';
 
 // Корень проекта как файловый путь
-const root = fileURLToPath(new URL(".", import.meta.url));
+const root = fileURLToPath(new URL('.', import.meta.url));
 
 export default defineConfig({
   plugins: [react(), svgr()],
@@ -16,37 +16,23 @@ export default defineConfig({
       '/api': {
         target: 'https://backend-cn5s.onrender.com', // Куда реально стучаться
         changeOrigin: true,
-        secure: false,      
+        secure: false,
       },
     },
   },
   resolve: {
     alias: {
       // Алиас для импорта из src (работает и в TS/JS, и в SCSS)
-      "@": path.resolve(root, "src")
-    }
+      '@': path.resolve(root, 'src'),
+    },
   },
   // Ничего про includePaths не нужно — Vite резолвит алиасы и в SCSS
   test: {
     projects: [
       {
         extends: true,
-        plugins: [
-          storybookTest({
-            configDir: path.join(root, ".storybook")
-          })
-        ],
-        test: {
-          name: "storybook",
-          browser: {
-            enabled: true,
-            headless: true,
-            provider: "playwright",
-            instances: [{ browser: "chromium" }]
-          },
-          setupFiles: [".storybook/vitest.setup.ts"]
-        }
-      }
-    ]
-  }
+        plugins: [],
+      },
+    ],
+  },
 });
