2bc9596488429fcbaa3acd09638533742b010fbf
Герман <german@MacBook-Air-German.local>
2026-02-22 21:27:26 +0300

    доделал страницу настроек

--- src/ui/components/BlueButton/BlueButton.tsx
+++ src/ui/components/BlueButton/BlueButton.tsx
@@ -1,34 +1,38 @@
-import React from "react";
-import styles from "./BlueButton.module.scss";
+import React from 'react';
+import style from './BlueButton.module.scss';
+import { useTheme } from '@/app/theme/useTheme';
 
 type BlueButtonProps = {
-    children: React.ReactNode;
-    onClick?: () => void;
-    Icon?: React.ComponentType<React.SVGProps<SVGSVGElement>>;
-    disabled?: boolean;
-    className?: string;
+  children: React.ReactNode;
+  onClick?: () => void;
+  Icon?: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+  disabled?: boolean;
+  className?: string;
 };
 
 export const BlueButton = ({
-    children,
-    onClick,
-    Icon,
-    disabled = false,
-    className = "",
+  children,
+  onClick,
+  Icon,
+  disabled = false,
+  className = '',
 }: BlueButtonProps) => {
-    return (
-        <button
-            onClick={onClick}
-            disabled={disabled}
-            className={`${styles.blueButton} ${className}`}
-        >
-            <span className={styles.text}>{children}</span>
+  const { theme } = useTheme();
+  return (
+    <div className={style[theme]}>
+      <button
+        onClick={onClick}
+        disabled={disabled}
+        className={`${style.blueButton} ${className}`}
+      >
+        <span className={style.text}>{children}</span>
 
-            {Icon && (
-                <span className={styles.icon}>
-                    <Icon />
-                </span>
-            )}
-        </button>
-    );
+        {Icon && (
+          <span className={style.icon}>
+            <Icon />
+          </span>
+        )}
+      </button>
+    </div>
+  );
 };
