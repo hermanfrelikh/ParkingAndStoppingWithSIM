97b91078d50e8036bf0b6fd6820421ca65828756
Герман <german@MacBook-Air-German.local>
2026-02-27 12:43:07 +0300

    for deploy

--- src/shared/assets/icons/index.ts
+++ src/shared/assets/icons/index.ts
@@ -1,7 +1,7 @@
 export { default as ArrowBackIcon } from './arrow-back.svg?react';
 export { default as NameIcon } from './name.svg?react';
 export { default as EmailIcon } from './email.svg?react';
-export { default as PasswordIcon } from './Password.svg?react';
+export { default as PasswordIcon } from './password.svg?react';
 export { default as PhoneNumberIcon } from './phone-number.svg?react';
 export { default as SubscriptionIcon } from './subscription.svg?react';
 export { default as HomeIcon } from './home.svg?react';
